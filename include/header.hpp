bdab67d8712ffb880ef3bfc35a1c609ce9c03433
MoraPresence <od34156la@mail.ru>
2020-04-03 08:20:52 -0700

    version 0.0.2

--- include/header.hpp
+++ include/header.hpp
@@ -21,6 +21,27 @@
 #include "root_certificates.hpp"
 #include <queue>
 #include <regex>
+#include <list>
+#include <mutex>
+#include <gumbo.h>
+#include <boost/beast.hpp>
+#include <iostream>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/error.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "root_certificates.hpp"
+#include <queue>
+#include <regex>
+#include <thread>
+
 using tcp = boost::asio::ip::tcp;
 namespace ssl = boost::asio::ssl;
 namespace http = boost::beast::http;
@@ -29,16 +50,23 @@ class crawler{
 public:
     std::list<std::string> getLinks(std::string&);
 
-    std::list<std::string> uniqueURL(std::list<std::string>&);
-
     void nesting(std::list<std::string>&);
 
-    std::string getHTML(std::smatch&);
+    std::string getHttp(std::smatch&);
+
+    std::string getHttps(std::smatch&);
 
     std::list<std::string> makeLinksList(std::string&);
 
+    void startThreadsDownload(std::list<std::string> &);
+
+    void downloader();
+
     void setCounter(unsigned i){
         _nestingCounter+=i;
+    }
+    void setLinkList(std::list<std::string> list){
+        listElementADD(_linkList, list);
     }
     unsigned getNestingVar(){
         return _nestingVar;
@@ -48,10 +76,15 @@ public:
         return _nestingCounter;
     }
 
-private:
-unsigned _nestingVar = 0;
-unsigned _nestingCounter = 0;
+    void listElementADD(std::list<std::string>&, std::list<std::string>&);
+
+//private:
+unsigned _nestingVar = 1;
+std::atomic_uint _nestingCounter = 0;
+unsigned _threadsCount = 0;
+std::size_t _threadCountDownload = std::thread::hardware_concurrency();
 std::list<std::string> _arrayList;
+std::list<std::string> _linkList;
 };
 #endif // INCLUDE_HEADER_HPP_
 
--- sources/source.cpp
+++ sources/source.cpp
@@ -2,48 +2,50 @@
 
 #include <header.hpp>
 int main() {
+    crawler cr;
     std::list<std::string> *uniqueList = new std::list<std::string>();
     std::list<std::string> *uniqueList2 = new std::list<std::string>();
-    uniqueList->push_back("bla");
-    uniqueList->push_back("bla");
-    uniqueList->push_back("bla");
+    uniqueList->push_back("bla1");
+    uniqueList->push_back("bla2");
+    uniqueList->push_back("bla3");
 
     for (std::string i : *uniqueList) {
         std::cout << i << std::endl;
     }
-    uniqueList->unique();
-    for (std::string i : *uniqueList) {
-        std::cout << i;
-    }
-    uniqueList2->merge(*uniqueList);
+    cr.listElementADD(*uniqueList2, *uniqueList);
     for (std::string i : *uniqueList2) {
         std::cout << i;
     }
-    crawler cr;
-    std::string a = "a";
-    std::ofstream ofs{ "/home/mora/Desktop/out.txt" }; // запись html-страницы в файл
-    std::list<std::string> uniqueList3 = cr.getLinks(a);
-    for (std::string i : uniqueList3) {
-         ofs << i << std::endl;
+
+    std::list<std::string> tmp;
+    tmp.push_back("https://yandex.ru/");
+    std::ofstream ofs{"/home/mora/Desktop/out.txt"}; // запись html-страницы в файл
+    cr.nesting(tmp);
+    cr._arrayList.sort();
+    cr._arrayList.unique();
+    for (std::string i : cr._arrayList) {
+        ofs << i << std::endl;
     }
     ofs.close();
 }
 
-void crawler::nesting(std::list<std::string> &) {
-    auto *tmp = new std::list<std::string>();
-    for (auto it = _arrayList.begin(); it != _arrayList.end(); ++it) {
-        tmp->merge(getLinks(*it));
+void crawler::nesting(std::list<std::string> &currentList) {
+    std::list<std::string> tmp;
+    for (auto it = currentList.begin(); it != currentList.end(); ++it) {
+        tmp.merge(getLinks(*it));
     }
     setCounter(1);
-    if (getNestingCounter() < getNestingVar()) {
-        nesting(*tmp);
+    tmp.sort();
+    tmp.unique();
+    this->listElementADD(_arrayList, tmp);
+    if (getNestingCounter() <= getNestingVar()) {
+        nesting(tmp);
     }
 }
 
-std::list<std::string> crawler::getLinks(std::string &) {
-    std::string sUri{"https://go.mail.ru/search?q=бла+&fm=1"}; // адрес исходной страницы
+std::list<std::string> crawler::getLinks(std::string &sUri) {
     std::regex rUri{"^(?:(https?)://)?([^/]+)(/.*)?"};
-
+    std::string sBody;
     std::smatch mr;
     if (std::regex_match(sUri, mr, rUri)) {
         for (auto const &sm : mr) {
@@ -52,35 +54,30 @@ std::list<std::string> crawler::getLinks(std::string &) {
     } else {
         std::cerr << "std::regex_match failed: " + sUri << "\n\n";
     }
-    if (mr[1].str() != "https") // работает пока только с https
+    if (mr[1].str() == "https") // работает пока только с https
     {
-        std::cerr << "mr[1].str() != \"https\"" << "\n\n";
+        sBody = getHttps(mr);
 
+    } else if (mr[1].str() == "http") {
+        sBody = getHttp(mr);
     }
-    std::string sBody = getHTML(mr);
+
 
     return makeLinksList(sBody);
 
 }
 
-std::string crawler::getHTML(std::smatch &mr) {
+std::string crawler::getHttp(std::smatch &mr) {
     std::string const host = mr[2];
-    std::string const port = "443"; // https - 443, http - 80
+    std::string const port = "80"; // https - 443, http - 80
     std::string const target = (mr[3].length() == 0 ? "/" : mr[3].str());
-    int version = 11; // или 10 для http 1.0
+    int version = 10; // или 10 для http 1.0
 
     boost::asio::io_context ioc;
-    ssl::context ctx{ssl::context::sslv23_client};
-    load_root_certificates(ctx);
     tcp::resolver resolver{ioc};
-    ssl::stream<tcp::socket> stream{ioc, ctx};
-    if (!SSL_set_tlsext_host_name(stream.native_handle(), host.c_str())) {
-        boost::system::error_code ec{static_cast<int>(::ERR_get_error()), boost::asio::error::get_ssl_category()};
-        throw boost::system::system_error{ec};
-    }
+    boost::beast::tcp_stream stream{ioc};
     auto const results = resolver.resolve(host, port);
-    boost::asio::connect(stream.next_layer(), results.begin(), results.end());
-    stream.handshake(ssl::stream_base::client);
+    stream.connect(results);
     http::request<http::string_body> req{http::verb::get, target, version};
     req.set(http::field::host, host);
     req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
@@ -91,8 +88,42 @@ std::string crawler::getHTML(std::smatch &mr) {
     return boost::beast::buffers_to_string(res.body().data());
 }
 
+std::string crawler::getHttps(std::smatch &mr) {
+    std::string const host = mr[2];
+    std::string const port = "443"; // https - 443, http - 80
+    std::string const target = (mr[3].length() == 0 ? "/" : mr[3].str());
+    int version = 11; // или 10 для http 1.0
+    try {
+        boost::asio::io_context ioc;
+        ssl::context ctx{ssl::context::sslv23_client};
+        load_root_certificates(ctx);
+        tcp::resolver resolver{ioc};
+        ssl::stream<tcp::socket> stream{ioc, ctx};
+        if (!SSL_set_tlsext_host_name(stream.native_handle(), host.c_str())) {
+            boost::system::error_code ec{static_cast<int>(::ERR_get_error()), boost::asio::error::get_ssl_category()};
+            throw boost::system::system_error{ec};
+        }
+        auto const results = resolver.resolve(host, port);
+        boost::asio::connect(stream.next_layer(), results.begin(), results.end());
+        stream.handshake(ssl::stream_base::client);
+        http::request<http::string_body> req{http::verb::get, target, version};
+        req.set(http::field::host, host);
+        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+        http::write(stream, req);
+        boost::beast::flat_buffer buffer;
+        http::response<http::dynamic_body> res;
+        http::read(stream, buffer, res);
+        return boost::beast::buffers_to_string(res.body().data());
+    } catch (...) {
+        std::cout << "error";
+        return "error";
+    }
+
+}
+
 std::list<std::string> crawler::makeLinksList(std::string &buf) {
-    std::list<std::string> *linksList = new std::list<std::string>;
+    std::list<std::string> linksList;
+
     GumboOutput *output = gumbo_parse(buf.c_str());
 
     std::queue<GumboNode *> qn;
@@ -106,7 +137,8 @@ std::list<std::string> crawler::makeLinksList(std::string &buf) {
             GumboAttribute *href = nullptr;
             if (node->v.element.tag == GUMBO_TAG_A &&
                 (href = gumbo_get_attribute(&node->v.element.attributes, "href"))) {
-                linksList->push_back(href->value);
+                if (std::string(href->value).find("http") == 0)
+                    linksList.push_back(href->value);
             }
 
             GumboVector *children = &node->v.element.children;
@@ -115,9 +147,37 @@ std::list<std::string> crawler::makeLinksList(std::string &buf) {
             }
         }
     }
-
     gumbo_destroy_output(&kGumboDefaultOptions, output);
-    linksList->unique();
-    return *linksList;
+    linksList.sort();
+    linksList.unique();
+    return linksList;
+}
+
+void crawler::listElementADD(std::list<std::string> &rx, std::list<std::string> &tx) {
+    for (auto it = tx.begin(); it != tx.end(); ++it) {
+        rx.push_back(*it);
+    }
+}
+
+void crawler::startThreadsDownload(std::list<std::string> &currentList) {
+    setLinkList(currentList);
+    if (_threadCountDownload >= 1 && _threadCountDownload
+                                     <= std::thread::hardware_concurrency()) {
+        std::vector<std::thread> threads;
+        threads.reserve(_threadCountDownload);
+        for (size_t i = 0; i < _threadCountDownload; ++i) {
+            threads.emplace_back();
+        }
+        for (auto &th : threads) {
+            th.join();
+        }
+    } else {
+        return;
+    }
+
+}
+
+void crawler::downloader() {
+
 }
 
